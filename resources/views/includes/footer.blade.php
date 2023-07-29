e59c18327605a6b3be45d9c450d900759d2bcec0
unknown <georgesroberto21@gmail.com>
2023-07-29 10:00:19 -0700

    Created pages  "header & footer" for object orientation(inheritance)

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,18 @@
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="{{ asset("vendor/apexcharts/apexcharts.js")}}"></script>
+<script src="{{ asset("vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+<script src="{{ asset("vendor/chart.js/chart.umd.js")}}"></script>
+<script src="{{ asset("vendor/echarts/echarts.min.js")}}"></script>
+<script src="{{ asset("vendor/quill/quill.min.js")}}"></script>
+<script src="{{ asset("vendor/simple-datatables/simple-datatables.js")}}"></script>
+<script src="{{ asset("vendor/tinymce/tinymce.min.js")}}"></script>
+<script src="{{ asset("vendor/php-email-form/validate.js")}}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset("js/main.js") }}"></script>
+
+</body>
+
+</html>
